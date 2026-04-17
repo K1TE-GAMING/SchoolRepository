1b5fb27f4e37867e29d3f0edf2e64bc85bbd5d56
K1TE-GAMING <bestgamerk21@gmail.com>
2026-04-17 17:44:27 +0800

    yes
    yes

--- Assets/Scripts/Weapons/BulletBehaviour.cs
+++ Assets/Scripts/Weapons/BulletBehaviour.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public float bulletSpeed = 10f;
+    public float bulletLifetime = 5f;
 
     void Update()
     {
         transform.position += transform.right * bulletSpeed * Time.deltaTime;
+
+        destroyBullet();
+    }
+
+    void destroyBullet()
+    {
+        if (bulletLifetime <= 0f)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            bulletLifetime -= Time.deltaTime;
+        }
     }
 }
